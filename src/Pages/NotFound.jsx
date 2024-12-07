abc8b03fff9b165042cd2a92960073c0bc233ba3
Qamrul-Hassan <sajal700@gmail.com>
2024-12-07 16:57:50 +0600

    c

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -8,22 +8,22 @@ const NotFound = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#F6F5FF] px-4">
       {/* 404 Heading */}
-      <h1 className="text-[80px] sm:text-[100px] font-extrabold text-pink-600 mb-4">
+      <h1 className="text-[80px] sm:text-[90px] font-extrabold text-pink-600 mb-2">
         404
       </h1>
 
       {/* Not Found Image */}
-      <div className="w-full flex justify-center mb-4">
+      <div className="w-full flex justify-center mb-2">
         <img
           src={NotFoundImage}
           alt="Not Found"
-          className="w-[70%] md:w-[600px] h-auto object-contain max-h-[350px] sm:max-h-[450px]"
+          className="w-[85%] md:w-[750px] h-auto object-contain max-h-[350px] sm:max-h-[450px]"
         />
       </div>
 
       {/* Back to Home Button */}
       <button
-        className="px-6 py-2 bg-pink-500 text-white font-medium rounded-lg hover:bg-pink-400 transition-all mb-8"
+        className="px-6 py-2 bg-pink-500 text-white font-medium rounded-lg hover:bg-pink-400 transition-all mb-4"
         onClick={() => navigate("/")}
       >
         Back to Home Page
